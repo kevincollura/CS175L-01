56021ae91b3f78896c1482b8831f654a7e1d2f47
kevincollura <114018408+kevincollura@users.noreply.github.com>
2023-02-07 20:29:46 -0500

    Add files via upload

---
+++ Restaurants.py
@@ -0,0 +1,44 @@
+'''
+Kevin Collura
+Professor Eckert
+CS-175L
+Restaurants
+'''
+
+
+
+vegetarian=False
+vegan=False
+glutenFree=False
+
+response1=input('Is anyone in your party a vegetarian? ')
+response1lower=response1.lower()
+if response1=='yes':
+    vegetarian=True
+
+response2=input('Is anyone in your party a vegan?')
+response2lower=response2.lower()
+if response2=='yes':
+    vegan=True
+
+response3=input('Is anyone in your party gluten free? ')
+response3lower=response3.lower()
+if response3=='yes':
+    glutenFree=True
+
+print('Here are your restaurant choices:')
+
+if not vegetarian and not vegan and glutenFree:
+    print("Joe's Gourmet Burger")
+
+elif not vegan and not glutenFree:
+    print("Mama's Fine Italian")
+
+elif not vegan:
+    print("Main Street Pizza")
+
+print("Corner Cafe")
+print("Chef's Kitchen")
+
+
+
