60e220c47b9e441ba268b302d7c903527d00c05e
kevincollura <114018408+kevincollura@users.noreply.github.com>
2023-03-06 13:17:12 -0500

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -1,26 +1,27 @@
 '''
 Kevin Collura
-AverageFromInput
+AverageFromInput.2
 CS-175L
 
 '''
 
     
 def main():
-    
-    numbers_file = open('numbers.txt', 'r')
-    total=0
-    count=0
-    
-    for line in numbers_file:
-        
-        total=total+float(line)
-        count=count+1
-        print (f'I read in {count} number(s) Current number is: {float(line)} Total is: {total}')
-    average=total/count
-    print(f'Average:',average)
-
-    numbers_file.close()
+    try:
+        numbers_file = open('numbers.txt', 'r')
+        total=0
+        count=0
+        for line in numbers_file:
+            total=total+float(line)
+            count=count+1
+            print (f'I read in {count} number(s) Current number is: {float(line)} Total is: {total}')
+            average=total/count
+            print(f'Average:',average)
+    except ValueError:
+            print("Error, invalid valu.")
+    except IOError:
+            print("File was not found.")
 
 if __name__ == '__main__':
     main()
+
