ff3c853a4269156e7795d36b01402cfbf3517792
kevincollura <114018408+kevincollura@users.noreply.github.com>
2023-04-23 20:53:45 -0400

    Add files via upload

---
+++ expensePieChart.py
@@ -0,0 +1,28 @@
+'''
+Kevin Collura
+CS-175L
+Expense Pie Chart
+'''
+
+
+
+import matplotlib.pyplot as plt
+
+
+def main():
+    file_name = open('expenses.txt', 'r')
+    expenses = file_name.readlines()
+    file_name.close
+
+    for x in range(len(expenses)):
+        expenses[i] = expenses[x].rstrip('\n')
+
+    sliceLabels = ['Rent', 'Gas', 'Food', 'Clothing', 'Car Payment', 'Misc.']
+
+    plt.pie(expenses, labels = sliceLabels)
+
+    plt.title('Monthly Expenses')
+
+    plt.show()
+
+main()
